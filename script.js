45cb685ac705735026dd7c7b26aa08b349ecd64a
Sara D'Arce <saradarce@gmail.com>
2023-12-31 16:12:37 -0800

    Increment counter recursively

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Declare counter
 
 let counter = 0;
+
+// Func incrementing counter recursively
+function incrementAndCall() {
+  counter++;
+  try {
+    incrementAndCall();
+  } catch (err) {
+    console.err("Error:", err);
+    console.log("Counter value:", counter);
+    setTimeout(() => {
+      // Reset counter
+      counter = 0;
+      trampoline(incrementAndCall);
+    }, 0);
+  }
+}
